f334166314854c8216cb22fa09eb853b854efaa7
oBregadiolli <bregadiolli.contato@gmail.com>
2024-12-17 15:19:09 -0300

    Toast no lugar de alert

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ document.getElementById('showToastBtn').addEventListener('click', function() {
         alert('Por favor, digite algo no input.');
     }
 });
+
+window.alert = function(message) {
+    var toastElement = document.querySelector('.toast');
+    var toast = new bootstrap.Toast(toastElement, {
+        autohide: true,
+        delay: 3000
+    });
+
+    // Define o corpo do toast para a mensagem do alerta
+    document.querySelector('.toast-body').textContent = message;
+
+    // Exibe o toast
+    toast.show();
+};
